e1b86f03c60521d0e01b0c6bce5560a63b92b5ab
Nuralim <nuralim@rds.co.id>
2026-02-19 11:16:22 +0700

    fix: schema prisma

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,4 +1,6 @@
-import 'dotenv/config';
+import * as dotenv from 'dotenv';
+dotenv.config({ path: '.env' });
+
 import makeWASocket, { DisconnectReason, useMultiFileAuthState, Browsers } from '@whiskeysockets/baileys';
 import { Boom } from '@hapi/boom';
 import { PrismaClient } from '@prisma/client';
